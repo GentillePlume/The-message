e1fbb478e813bfff33558e06c600737989afb4bc
Gentille Plume <melvin.lemoine@feather-project.org>
2018-06-17 12:36:19 +0200

    Reduced typeSpeed for mobile browsers

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var typeSpeed = 100;
 
 //Detect if the user is using a mobile
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
- var typeSpeed = 25;
+ var typeSpeed = 1;
     alert("MOBILE BROWSER DETECTED");
 }
 
